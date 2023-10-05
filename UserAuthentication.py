9f078970152ab1f9d9c5611819965ef51a2e4730
EhtishamUlHassan <muhammadjalalkhan7658@gmail.com>
2023-10-05 13:40:38 +0500

    Connected DB with the frontend application.

--- UserAuthentication.py
+++ UserAuthentication.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
+import mysql.connector
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'  # Replace with a secure secret key
 
-# Replace the database-related code with user management logic
-
-# Sample data structure to store user information (replace with a proper database)
-users = []
+# Create and configure MySQL database connection
+db = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="1328",
+    database="myca4"
+)
 
 class User:
-    def __init__(self, username, password):
-        self.id = len(users) + 1
+    def __init__(self, id, username, password):
+        self.id = id
         self.username = username
-        self.password = generate_password_hash(password)
-
-# Dummy data for testing (replace with actual database interactions)
-users.append(User('user1', 'password1'))
-users.append(User('user2', 'password2'))
+        self.password = password
 
 @app.route('/')
 def index():
@@ -29,19 +29,24 @@ def signup():
         username = request.form['username']
         password = request.form['password']
 
-        # Check if the username already exists
-        existing_user = next((user for user in users if user.username == username), None)
+        cursor = db.cursor()
+
+        # Check if the username already exists in the database
+        cursor.execute('SELECT * FROM user WHERE username = %s', (username,))
+        existing_user = cursor.fetchone()
 
         if existing_user:
             flash('Username already exists. Please choose a different one.', 'danger')
         else:
-            # Create a new user and add them to the user list (replace with database code)
-            new_user = User(username, password)
-            users.append(new_user)
+            # Create a new user and add them to the database
+            hashed_password = generate_password_hash(password)
+            cursor.execute('INSERT INTO user (username, password) VALUES (%s, %s)',
+                           (username, hashed_password))
+            db.commit()
 
             flash('Signup successful! You can now log in.', 'success')
             return redirect(url_for('login'))
-    
+
     return render_template('signup.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -50,10 +55,15 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        # Check if the username and password match (replace with database code)
-        user = next((user for user in users if user.username == username), None)
+        cursor = db.cursor()
 
-        if user and check_password_hash(user.password, password):
+        # Retrieve user from the database by username
+        cursor.execute('SELECT * FROM user WHERE username = %s', (username,))
+        user_row = cursor.fetchone()
+
+        if user_row and check_password_hash(user_row[2], password):  # Assuming password hash is in the third column
+            user = User(*user_row)
+
             # Store the user's ID in the session to track the login state
             session['user_id'] = user.id
             flash('Login successful!', 'success')
